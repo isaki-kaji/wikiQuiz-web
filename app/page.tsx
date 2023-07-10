35d37b296adc0862b778b3ac283d9513e572f99f
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2023-07-10 18:17:05 +0900

    CategoryCard作成中

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import CategoryCard from "./components/home/CategoryCard";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="pt-[61px] sm:pt-[73px]">
+      <div className="flex flex-col items-center h-screen py-6">
+        <CategoryCard />
+      </div>
+    </div>
+  );
 }
