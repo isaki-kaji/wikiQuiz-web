bd7d62d49fb24e5078af3434eb2dbb34ea40f4a0
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2023-07-12 01:00:26 +0900

    CategoryCard作成中

--- app/components/home/CategoryCard.tsx
+++ app/components/home/CategoryCard.tsx
@@ -1,7 +1,19 @@
-const CategoryCard: React.FC = () => {
+import { IconContext, IconType, IconBaseProps } from "react-icons";
+
+type CategoryCardProps = {
+  title: string;
+  Icon: IconType;
+};
+
+const CategoryCard: React.FC<CategoryCardProps> = ({ title, Icon }) => {
   return (
     <div className="w-5/6 h-1/3 sm:h-[250px] sm:w-[250px] border-[1px] rounded-lg cursor-pointer transition duration-300 ease-in-out hover:shadow-lg dark:hover:shadow-black/30">
-      big card
+      <div className="flex flex-col h-full justify-center items-center">
+        <h2 className="text-2xl">{title}</h2>
+        <div>
+          <Icon color="#22A699" size={"80px"} />
+        </div>
+      </div>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import CategoryCard from "./components/home/CategoryCard";
+import { CiBaseball } from "react-icons/ci";
 
 export default function Home() {
   return (
     <div className="pt-[61px] sm:pt-[73px]">
       <div className="flex flex-col items-center h-screen py-6">
-        <CategoryCard />
+        <CategoryCard title="プロ野球" Icon={CiBaseball} />
       </div>
     </div>
   );
