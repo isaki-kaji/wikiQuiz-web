e8cbf143739a10b6cfcfc32d9254328e55ee1118
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2023-07-12 20:45:57 +0900

    CategoryCardをmapして表示

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import { categoryList } from "./category-list";
 import CategoryCard from "./components/home/CategoryCard";
-import { CiBaseball } from "react-icons/ci";
 
 export default function Home() {
   return (
-    <div className="pt-[61px] sm:pt-[73px]">
-      <div className="flex flex-col items-center h-screen py-6">
-        <CategoryCard title="プロ野球" Icon={CiBaseball} />
+    <div className="pt-[61px] sm:pt-[73px] h-full">
+      <div className="py-2 sm:py-4 flex sm:grid flex-col gap-2 justify-center items-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:place-content-center">
+        {categoryList.map((category) => {
+          return (
+            <CategoryCard
+              title={category.title}
+              Icon={category.Icon}
+            ></CategoryCard>
+          );
+        })}
       </div>
     </div>
   );
