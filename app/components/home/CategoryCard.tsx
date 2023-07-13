b065b112471466a4694a35dcc1143025876a2cd3
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2023-07-13 21:32:09 +0900

    CategoryCardをflex-wrapで並べた

--- app/components/home/CategoryCard.tsx
+++ app/components/home/CategoryCard.tsx
@@ -2,7 +2,7 @@ import { CategoryCardProps } from "@/app/category-list";
 
 const CategoryCard: React.FC<CategoryCardProps> = ({ title, Icon }) => {
   return (
-    <div className="w-5/6 h-1/3 sm:h-[250px] sm:w-[250px] border-[1px] rounded-lg cursor-pointer transition duration-300 ease-in-out hover:shadow-lg dark:hover:shadow-black/30">
+    <div className="h-[160px] w-[160px] sm:h-[250px] sm:w-[250px] border-[1px] rounded-lg cursor-pointer transition duration-300 ease-in-out hover:shadow-lg dark:hover:shadow-black/30">
       <div className="flex flex-col gap-2 h-full justify-center items-center">
         <h2 className="text-2xl select-none">{title}</h2>
         <div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import CategoryCard from "./components/home/CategoryCard";
 
 export default function Home() {
   return (
-    <div className="pt-[61px] sm:pt-[73px] h-full">
-      <div className="py-2 sm:py-4 flex sm:grid flex-col gap-2 justify-center items-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:place-content-center">
+    <div className="pt-[61px] sm:pt-[73px] h-full px-4">
+      <div className="py-4 sm:py-6 flex flex-wrap gap-5 justify-center items-center">
         {categoryList.map((category) => {
           return (
             <CategoryCard
