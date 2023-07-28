5c24817b4439d2991db4c5306471c8992d35e305
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2023-07-28 16:45:31 +0900

    CategoryCardでfirestoreからデータの取得に成功

--- app/components/home/CategoryCard.tsx
+++ app/components/home/CategoryCard.tsx
@@ -1,4 +1,3 @@
-import { type } from "os";
 import React, { useEffect, useState } from "react";
 import { AiOutlineCrown } from "react-icons/ai";
 import { doc, getDoc } from "firebase/firestore";
@@ -17,22 +16,28 @@ const CategoryCard = ({ category }: CategoryCardProps) => {
   const [king, setKing] = useState<King>({ name: "挑戦者求む", score: 0 });
 
   const getDocument = async () => {
-    const docRef = doc(db, "king", category);
+    const docRef = doc(db, "King", category);
     const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      setKing(docSnap.data() as King);
+    }
   };
 
   useEffect(() => {
     getDocument();
   }, []);
+
   return (
     <div className="w-[90%] flex flex-col border-b border-slate-200 pb-2 group cursor-pointer">
       <h4 className="text-base sm:text-lg py-2 group-hover:text-[#F2BE22] select-none">
         {category}
       </h4>
-      <div className="flex flex-between">
-        <AiOutlineCrown />
-        <p>name:</p>
-        <p>score:</p>
+      <div>
+        <div className="flex flex-between px-2">
+          <AiOutlineCrown />
+          <p>name: {king.name}</p>
+          <p>score: {king.score}</p>
+        </div>
       </div>
     </div>
   );
