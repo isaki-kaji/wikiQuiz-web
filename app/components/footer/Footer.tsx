cf1426b0f4fa05e99be6717c65aa3ba407d2e8f8
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2023-08-03 23:22:19 +0900

    簡易footer完成

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="text-white bg-[#22A699] mt-4 sm:mt-8 absolute b-0 w-full">
+      <div className="p-4 flex justify-end gap-6">
+        <p>©kaji-isaki</p>
+        <a href="https://icons8.com">
+          <p>Icons by Icons8</p>
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
