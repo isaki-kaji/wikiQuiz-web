d315f37b401b1b9cc3df515ac8b202671ebb110f
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2023-08-06 14:49:25 +0900

    AnswerBar pc版のレイアウトはよし

--- app/components/play/AnswerBar.tsx
+++ app/components/play/AnswerBar.tsx
@@ -9,28 +9,34 @@ const AnswerBar = () => {
   const [selectedAnswer, setSelectedAnswer] = useState("");
   const { titleList } = useQuizInfoStore();
 
-  const filteredAnswers = titleList.filter((title) => {
-    return title.toLowerCase().includes(query.toLowerCase());
-  });
+  const filteredAnswers = titleList
+    .filter((title) => title.toLowerCase().includes(query.toLowerCase()))
+    .slice(0, 3);
 
   return (
-    <div className="absolute bottom-20 left-1/2 transform -translate-x-1/2 md:bottom-40  border-2 border-[#22A699]">
-      <Combobox value={selectedAnswer} onChange={setSelectedAnswer}>
-        <Combobox.Input onChange={(event) => setQuery(event.target.value)} />
-        <Combobox.Options>
-          {query === ""
-            ? null
-            : filteredAnswers.map((answer) => (
-                <Combobox.Option
-                  key={answer}
-                  value={answer}
-                  className="cursor-pointer"
-                >
-                  {answer}
-                </Combobox.Option>
-              ))}
-        </Combobox.Options>
-      </Combobox>
+    <div className="absolute bottom-[8rem] sm:bottom-[9rem] left-1/2 transform -translate-x-1/2">
+      <div className="relative">
+        <h3 className="text-center">あなたの解答</h3>
+        <Combobox value={selectedAnswer} onChange={setSelectedAnswer}>
+          <Combobox.Input
+            onChange={(event) => setQuery(event.target.value)}
+            className="border-2 border-[#22A699] w-[250px] h-[60px] text-lg text-center"
+          />
+          <Combobox.Options className="absolute mt-1 w-full">
+            {query === ""
+              ? null
+              : filteredAnswers.map((answer) => (
+                  <Combobox.Option
+                    key={answer}
+                    value={answer}
+                    className="cursor-pointer p-2 border-x border-b border-[#22A699] w-[250px]"
+                  >
+                    {answer}
+                  </Combobox.Option>
+                ))}
+          </Combobox.Options>
+        </Combobox>
+      </div>
     </div>
   );
 };
