da809cb3a5739a388d7ab824fac5d00faefaead6
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2023-08-08 00:12:56 +0900

    微修正

--- app/components/play/AnswerBar.tsx
+++ app/components/play/AnswerBar.tsx
@@ -14,34 +14,36 @@ const AnswerBar = () => {
     .slice(0, 3);
 
   return (
-    <div className="flex items-end">
-      <div className="relative">
-        <h3 className="text-center">あなたの解答</h3>
-        <Combobox value={selectedAnswer} onChange={setSelectedAnswer}>
-          <Combobox.Input
-            onChange={(event) => setQuery(event.target.value)}
-            className="border-2 border-[#22A699] w-[250px] h-[60px] text-lg text-center"
-          />
-          <Combobox.Options className="absolute mt-1 w-full">
-            {query === ""
-              ? null
-              : filteredAnswers.map((answer) => (
-                  <Combobox.Option
-                    key={answer}
-                    value={answer}
-                    className="cursor-pointer p-2 border-x border-b border-[#22A699] w-[250px]"
-                  >
-                    {answer}
-                  </Combobox.Option>
-                ))}
-          </Combobox.Options>
-        </Combobox>
-      </div>
-      <div
-        className="flex-center h-[60px] w-[100px] bg-[#540375] hover:opacity-70 cursor-pointer"
-        onClick={() => {}}
-      >
-        <div className=" text-white">{query === "" ? "パス" : " 答"}</div>
+    <div className="absolute bottom-[8rem] sm:bottom-[10rem] left-1/2 transform -translate-x-1/2 z-10">
+      <div className="flex items-end">
+        <div className="relative">
+          <h3 className="text-center">あなたの解答</h3>
+          <Combobox value={selectedAnswer} onChange={setSelectedAnswer}>
+            <Combobox.Input
+              onChange={(event) => setQuery(event.target.value)}
+              className="border-2 border-[#22A699] w-[250px] h-[60px] text-lg text-center"
+            />
+            <Combobox.Options className="absolute mt-1 w-full">
+              {query === ""
+                ? null
+                : filteredAnswers.map((answer) => (
+                    <Combobox.Option
+                      key={answer}
+                      value={answer}
+                      className="cursor-pointer p-2 border-x border-b border-[#22A699] w-[250px]"
+                    >
+                      {answer}
+                    </Combobox.Option>
+                  ))}
+            </Combobox.Options>
+          </Combobox>
+        </div>
+        <div
+          className="flex-center h-[60px] w-[100px] bg-[#540375] hover:opacity-70 cursor-pointer"
+          onClick={() => {}}
+        >
+          <div className=" text-white">{query === "" ? "パス" : " 答"}</div>
+        </div>
       </div>
     </div>
   );
--- app/play/page.tsx
+++ app/play/page.tsx
@@ -3,13 +3,14 @@ import { Suspense } from "react";
 import QuizTextCard from "../components/play/QuizTextCard";
 import Loading from "../Loading";
 import InputInterface from "../components/play/InputInterface";
+import AnswerBar from "../components/play/AnswerBar";
 
 const PlayPage = () => {
   return (
     <Suspense fallback={<Loading />}>
       <div className="relative h-screen bg-gray-100">
         <QuizTextCard />
-        <InputInterface />
+        <AnswerBar />
       </div>
     </Suspense>
   );
