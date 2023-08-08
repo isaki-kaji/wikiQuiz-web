560471b8ef5ef314e52d2f8a44a25bd2ddb2b9a8
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2023-08-08 18:47:39 +0900

    QuizTextIndexのボタンを作成

--- app/components/play/QuizTextCard.tsx
+++ app/components/play/QuizTextCard.tsx
@@ -5,14 +5,14 @@ import { useQuizInfoStore } from "@/app/stores/quizInfoStore";
 import { useQuizStore } from "@/app/stores/quizStore";
 import { doc, getDoc } from "firebase/firestore";
 import { useEffect } from "react";
+import CustomButton from "../common/CustomButton";
 
-const quizTextCard = () => {
+const QuizTextCard = () => {
   const { shuffledTitleList, category } = useQuizInfoStore();
   const {
     quizTexts,
     setQuizTexts,
     quizIndex,
-    incrementQuizIndex,
     quizTextIndex,
     incrementQuizTextIndex,
   } = useQuizStore();
@@ -30,12 +30,23 @@ const quizTextCard = () => {
   }, [quizTextIndex]);
 
   return (
-    <div className="absolute top-10 md:top-20 left-1/2 transform -translate-x-1/2 w-[95%] sm:w-[70%] lg:w-[800px] bg-white h-[60%] shadow-2xl rounded-lg">
-      <p className="p-6 sm:p-10 lg:px-16 lg:py-20 text-base sm:text-lg md:text-xl">
-        {quizTexts[quizTextIndex]}
-      </p>
+    <div className="absolute top-10 md:top-20 left-1/2 transform -translate-x-1/2 w-[95%] sm:w-[70%] lg:w-[800px] bg-white h-[60%] shadow-2xl rounded-lg ">
+      <div className="relative h-full overflow-auto">
+        <p className="p-6 sm:p-10 lg:px-16 lg:py-20 text-base sm:text-lg md:text-xl">
+          {quizTexts[quizTextIndex]}
+        </p>
+      </div>
+      <div className="absolute bottom-4 right-4">
+        <CustomButton
+          title={"→"}
+          containerStyles="rounded-full text-white bg-[#22A669] hover:opacity-70 z-20"
+          handleClick={() => {
+            incrementQuizTextIndex();
+          }}
+        />
+      </div>
     </div>
   );
 };
 
-export default quizTextCard;
+export default QuizTextCard;
